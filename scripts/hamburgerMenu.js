e264d147becf8a639ec734920193b2a488733878
MINOR Thomas <mthomas.pro@outlook.fr>
2022-07-09 17:01:03 +0200

    v0.0.5: change mobile navbar & add transition after open and close

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -1,34 +1,36 @@
-const btn = document.querySelector(".header_navbar_links_menu");
-const navbar = document.querySelector(".header_navbar_links_list");
+const btn = document.querySelector(".header_navbar_button");
+const navbarLinks = document.querySelector(".header_navbar_links");
+
+const hamburgerMenuLines = `
+    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="0" height="0">
+        <path
+            d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z" />
+    </svg>
+`
+const hamburgerMenuCross = `
+    <svg fill="#c2c2c2" width="15px" height="15px" viewBox="0 0 15 15" version="1.1" id="cross" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2.64,1.27L7.5,6.13l4.84-4.84C12.5114,1.1076,12.7497,1.0029,13,1c0.5523,0,1,0.4477,1,1&#xA;&#x9;c0.0047,0.2478-0.093,0.4866-0.27,0.66L8.84,7.5l4.89,4.89c0.1648,0.1612,0.2615,0.3796,0.27,0.61c0,0.5523-0.4477,1-1,1&#xA;&#x9;c-0.2577,0.0107-0.508-0.0873-0.69-0.27L7.5,8.87l-4.85,4.85C2.4793,13.8963,2.2453,13.9971,2,14c-0.5523,0-1-0.4477-1-1&#xA;&#x9;c-0.0047-0.2478,0.093-0.4866,0.27-0.66L6.16,7.5L1.27,2.61C1.1052,2.4488,1.0085,2.2304,1,2c0-0.5523,0.4477-1,1-1&#xA;&#x9;C2.2404,1.0029,2.4701,1.0998,2.64,1.27z"/>
+    </svg>
+`
+
+btn.innerHTML = hamburgerMenuLines;
 
 btn.addEventListener("click", function () {
     const btnActive = btn.getAttribute("active");
 
     if (btnActive === "false") {
-        navbar.style.display = "flex";
+        navbarLinks.style.height = "100%";
+
         btn.setAttribute("active", "true");
+        btn.style.zIndex = "2"
         btn.innerHTML = hamburgerMenuCross;
-    }
-    if (btnActive === "true") {
-        navbar.style.display = "none";
+    } else if (btnActive === "true") {
+        navbarLinks.style.height = "0%"
+
         btn.setAttribute("active", "false");
         btn.innerHTML = hamburgerMenuLines;
+        btn.style.zIndex = "0"
+    } else {
+        throw "Navbar attribute error: Active attribute not found";
     }
 })
-
-const hamburgerMenuLines = `
-    <svg class="hamburger_svg" viewBox="0 0 18 15">
-        <path fill="#424242"
-            d="M18,1.484c0,0.82-0.665,1.484-1.484,1.484H1.484C0.665,2.969,0,2.304,0,1.484l0,0C0,0.665,0.665,0,1.484,0 h15.031C17.335,0,18,0.665,18,1.484L18,1.484z" />
-        <path fill="#424242"
-            d="M18,7.516C18,8.335,17.335,9,16.516,9H1.484C0.665,9,0,8.335,0,7.516l0,0c0-0.82,0.665-1.484,1.484-1.484 h15.031C17.335,6.031,18,6.696,18,7.516L18,7.516z" />
-        <path fill="#424242"
-            d="M18,13.516C18,14.335,17.335,15,16.516,15H1.484C0.665,15,0,14.335,0,13.516l0,0 c0-0.82,0.665-1.484,1.484-1.484h15.031C17.335,12.031,18,12.696,18,13.516L18,13.516z" />
-    </svg>
-`
-
-const hamburgerMenuCross = `
-    <svg width="15px" height="15px" viewBox="0 0 15 15" version="1.1" id="cross" xmlns="http://www.w3.org/2000/svg">
-        <path d="M2.64,1.27L7.5,6.13l4.84-4.84C12.5114,1.1076,12.7497,1.0029,13,1c0.5523,0,1,0.4477,1,1&#xA;&#x9;c0.0047,0.2478-0.093,0.4866-0.27,0.66L8.84,7.5l4.89,4.89c0.1648,0.1612,0.2615,0.3796,0.27,0.61c0,0.5523-0.4477,1-1,1&#xA;&#x9;c-0.2577,0.0107-0.508-0.0873-0.69-0.27L7.5,8.87l-4.85,4.85C2.4793,13.8963,2.2453,13.9971,2,14c-0.5523,0-1-0.4477-1-1&#xA;&#x9;c-0.0047-0.2478,0.093-0.4866,0.27-0.66L6.16,7.5L1.27,2.61C1.1052,2.4488,1.0085,2.2304,1,2c0-0.5523,0.4477-1,1-1&#xA;&#x9;C2.2404,1.0029,2.4701,1.0998,2.64,1.27z"/>
-    </svg>
-`
